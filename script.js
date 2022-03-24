227d2aedf1aed4cf437979b4c01ff69f4b842e42
Bakare Faruq <bakarefaruq.a@gmail.com>
2022-03-24 20:32:55 +0100

    desktop completed

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ shareBtn.addEventListener("click", () => {
   tooltip.classList.toggle("active");
   shareBtn.classList.toggle("active");
 });
+
+shareBtn.addEventListener("blur", (e) => {
+  // When a mouse click is detected outside the tooltip, the tooltip is closed
+  // e.target is the element that triggered the event
+  // e.relatedTarget is the element that was clicked
+  console.log(e.target, e.relatedTarget);
+  if (e.relatedTarget !== tooltip) {
+    tooltip.classList.remove("active");
+    shareBtn.classList.remove("active");
+  }
+});
+tooltip.addEventListener("blur", (e) => {
+  tooltip.classList.remove("active");
+  shareBtn.classList.remove("active");
+});
